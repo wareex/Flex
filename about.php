96f949cd791e34adcc562abe9cd1ce10f8342cea
Busari Wareez <wareezbusari@gmail.com>
2022-12-24 19:44:37 +0100

    php info in about

--- about.php
+++ about.php
@@ -20,6 +20,7 @@
         <div class="first">
             <h1> About Us</h1>
             <p>Purpose of the association 1. The association pursues exclusively and directly charitable purposes within the meaning of "Tax-privileged purposes" section of the Fiscal Code (AO). 2. The purpose of the association is the promotion of youth and elderly care within the meaning of §52 paragraph 2 No. 4 AO. 3. The purpose of the statutes is realized in particular through the following activities: 3.1 To reduce the unemployment rate among young people and people and to increase poverty alleviate. 3.2 Support for the development of young people from Africa and their Integration into the European system and German culture. 3.3 via interactive platforms in international activities such as conferences, Symposia, educational forums and similar events have moral goals and Instilling values in young people. 3.4 Involvement of students/youth in educational and social programs, that enable a better future, e.g. B. through vocational training / entrepreneurship and IT training. 3.5 Maximize youth potential, promote self-sufficiency and contribute to financial security. 3.6 Raising funds for the implementation of community projects for Improving the lives of young people.</p>
+            <?php phpinfo(); ?>
         </div>
     </div>
 

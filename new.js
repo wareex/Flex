05c20383f06399c4ee59a7111c74a887cf563274
Busari Wareez <wareezbusari@gmail.com>
2022-12-28 17:36:00 +0100

    wait

--- new.js
+++ new.js
@@ -1,8 +1,52 @@
 
-var sodiki = 45, tall = "sop" , plr2 = 56.7;
+var sodiki = "45", tall = "sop" , plr2 = 56.7;
 // 
 var waht;
-
     waht= "whener";
+// structure
+       // alert(sodiki);
+       // alert(tall);
+       // alert(plr2);
+       // alert(waht);
 
-        alert(sodiki);
+
+//console.log()
+
+//conditional
+var shola = 48;
+var Ade = "57.78";
+
+if (shola == Ade){
+    //alert('Yes');
+} else {
+    //alert("No");
+}
+
+//Teneray operator
+var buket = shola > Ade ? shola : Ade;
+//alert(buket); 
+
+//Oprands (second class)
+var a = 21;
+--a;
+alert(a);
+
+
+//loops
+
+var b =11;
+while(b<=10){
+alert("Happy birday to Aziz");
+b++;
+} 
+
+var c = 6;
+do {
+  //  alert('IIIRG is the best');
+    c++;
+} while (c<5);
+
+for(var e = 1; e < 6; e++){
+
+ alert('Till we all win');
+}
